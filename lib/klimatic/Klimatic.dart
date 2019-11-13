00cc77d5a9fd92ad4189854ceea5bb437102b15e
AsiriRajapaksha <asiri.l.rajapaksha59@gmail.com>
2019-11-13 17:00:41 +0530

    second commit

--- lib/klimatic/Klimatic.dart
+++ lib/klimatic/Klimatic.dart
@@ -11,8 +11,37 @@ class _KlimaticState extends State<Klimatic> {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Klimatic"),
-        backgroundColor: Colors.white,
-        actions: <Widget>[],
+        centerTitle: true,
+        backgroundColor: Colors.red,
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(Icons.menu),
+              onPressed: ()=>{debugPrint("hey")}
+              ),
+        ],
+      ),
+      body: new Stack(
+        children: <Widget>[
+          new Center(
+            child: new Image.asset('images/umbrella.png',
+            width: 500.0,
+            height: 1000.0,
+            fit: BoxFit.fill,),
+          ),
+          new Container(
+            alignment: Alignment.topRight,
+            margin: const EdgeInsets.fromLTRB(0.0, 10.9, 20.0, 0.0),
+            child: new Text("Spokane" , style : new TextStyle(color: Colors.white , fontSize: 23.0 , fontStyle: FontStyle.italic)),
+          ),
+          new Container(
+            alignment: Alignment.center,
+            child: new Image.asset('images/light_rain.png'),
+          ),
+          new Container(
+            margin: const EdgeInsets.fromLTRB(20.0, 450.0, 0.0, 0.0),
+            child : new Text("67.5F" , style: new TextStyle(color: Colors.white , fontSize: 50.0 , fontWeight: FontWeight.w500),),
+          )
+        ],
       ),
     );
   }
